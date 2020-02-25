e15b9eba7447fbab18c8641ae267ddcc9f820db6
lawal Samuel <samthedonz@gmail.com>
2020-02-25 13:48:02 +0100

    update bvn added

--- ussdModules/referrals.js
+++ ussdModules/referrals.js
@@ -1,43 +1,43 @@
-const { verifyUser, textfunc, message, scheme } = require('./buddyFuctions');
+const { verifyUser, textfunc, message, scheme } = require("./buddyFuctions");
 
 module.exports = {
-	referalState(menu) {
-		menu.state('referal', {
-			run: () => {
-				menu.con(`Refer a friend \n Enter your friends number:`);
-			},
-			next: {
-				'*[0-9]': () => {
-					return new Promise((resolve) => {
-						menu.session.get('data').then((val) => {
-							let text =
-								'Your friend ' +
-								val.fName +
-								' ' +
-								val.sName +
-								' has invited you to use ' +
-								scheme +
-								'.\nPlease visit http://spectrum.rubikpay.tech/ and start earning!!\nRegards, The ' +
-								scheme +
-								' Team';
-							let phoneNumber = '+234' + menu.val.substring(1);
-							message(text, phoneNumber).then((res) => {
-								console.log(res.data.messages);
-								resolve('referal.status');
-							});
-						});
-					});
-				},
-				'0': 'welcome'
-			}
-		});
-		menu.state('referal.status', {
-			run: () => {
-				menu.con('Sent. Thank you \n Press 0 to go back');
-			},
-			Next: {
-				'0': 'welcome'
-			}
-		});
-	}
+  referalState(menu) {
+    menu.state("referal", {
+      run: () => {
+        menu.con(`Refer a friend \n Enter your friends number:`);
+      },
+      next: {
+        "*[0-9]": () => {
+          return new Promise(resolve => {
+            menu.session.get("data").then(val => {
+              let text =
+                "Your friend " +
+                val.fName +
+                " " +
+                val.sName +
+                " has invited you to use " +
+                scheme +
+                ".\nPlease visit http://spectrum.rubikpay.tech/ and start earning!!\nRegards, The " +
+                scheme +
+                " Team";
+              let phoneNumber = "+234" + menu.val.substring(1);
+              message(text, phoneNumber).then(res => {
+                console.log(res.data.requestError.serviceException);
+                resolve("referal.status");
+              });
+            });
+          });
+        },
+        "0": "welcome"
+      }
+    });
+    menu.state("referal.status", {
+      run: () => {
+        menu.con("Sent. Thank you \n Press 0 to go back");
+      },
+      Next: {
+        "0": "welcome"
+      }
+    });
+  }
 };
--- ussdModules/welcome.js
+++ ussdModules/welcome.js
@@ -1,59 +1,55 @@
-const { verifyPin, scheme } = require('./buddyFuctions');
+const { verifyPin, scheme } = require("./buddyFuctions");
 module.exports = {
-	setBaseUser: [],
-	welcomeState(menu) {
-		menu.state('welcomeState', {
-			run: () => {
-				menu.session.get('loginStatus').then((status) => {
-					console.log(status, 'login status');
-					if (status !== '') {
-						menu.con(
-							'Welcome to ' +
-								scheme +
-								'\n1. Loans \n2. Check balance \n3. Fund Wallet \n4. Referrals \n0. Exit '
-						);
-					} else {
-						verifyPin(menu.val, menu.args.phoneNumber).then((val) => {
-							menu.session.set('data', val.data);
-							this.setBaseUser = val.data;
-							// menu.session.get('data').then((val) => {
-							// 	console.log(val, 'data from the welcome screen');
-							// });
-							// 	console.log(val.data._id, 'id')
-							
-							if (val.data._id !== undefined) {
-								console.log('NOT EMPTY')
-								menu.session.set('loginStatus', 'isLogedIn');
-								menu.con(
-									'Welcome to ' +
-										scheme +
-										'\n1. Loans \n2. Check bal \n3. Fund Wallet \n4. Referrals \n0. Exit '
-								);
-							} else {
-								menu.session.set('loginStatus', '');
-								console.log('is empty')
-								menu.con('Please login again. Enter a valid PIN');
-							}
-						});
-					}
-				});
-			},
-			next: {
-				'1': 'loan',
-				'2': 'checkBalance',
-				'3': 'fundWalletState',
-				'4': 'referal',
-				'0': 'exit',
-				'*[1-9]': 'welcomeState'
-			}
-		});
+  setBaseUser: [],
+  welcomeState(menu) {
+    menu.state("welcomeState", {
+      run: () => {
+        menu.session.get("loginStatus").then(status => {
+          console.log(status, "login status");
+          if (status !== "") {
+            menu.con(
+              "Welcome to " +
+                scheme +
+                "\n1. Loans \n2. Check balance \n3. Fund Wallet \n4. Referrals \n0. Exit "
+            );
+          } else {
+            verifyPin(menu.val, menu.args.phoneNumber).then(val => {
+              menu.session.set("data", val.data);
+              console.log(val.data, "bvn");
+              this.setBaseUser = val.data;
+              if (val.data._id !== undefined) {
+                console.log("NOT EMPTY");
+                menu.session.set("loginStatus", "isLogedIn");
+                menu.con(
+                  "Welcome to " +
+                    scheme +
+                    "\n1. Loans \n2. Check bal \n3. Fund Wallet \n4. Referrals \n0. Exit "
+                );
+              } else {
+                menu.session.set("loginStatus", "");
+                console.log("is empty");
+                menu.con("Please login again. Enter a valid PIN");
+              }
+            });
+          }
+        });
+      },
+      next: {
+        "1": "loan",
+        "2": "checkBalance",
+        "3": "fundWalletState",
+        "4": "referal",
+        "0": "exit",
+        "*[1-9]": "welcomeState"
+      }
+    });
 
-		menu.state('exit', {
-			run: () => {
-				menu.session.set('loginStatus', '').then((val) => {
-					menu.end('Thanks for using ' + scheme);
-				});
-			}
-		});
-	}
+    menu.state("exit", {
+      run: () => {
+        menu.session.set("loginStatus", "").then(val => {
+          menu.end("Thanks for using " + scheme);
+        });
+      }
+    });
+  }
 };
