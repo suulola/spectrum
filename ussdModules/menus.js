4ffe7a365b2994744e6744901207e4673e6730b2
drtobbyas <dr.tobbyas@gmail.com>
2020-06-29 15:59:12 +0100

    fix:

--- ussdModules/menus.js
+++ ussdModules/menus.js
@@ -1,47 +1,49 @@
-const { checkIfUserExists } = require('./buddyFuctions');
-const { RegisterUser } = require('./registeration');
-const { referalState } = require('./referrals');
-const { checkBalanceState } = require('./checkbalance');
-const { loanState } = require('./loan');
-const { welcomeState } = require('./welcome');
-const { fundWalletState } = require('./fundWallet');
-const { payBackLoanState } = require('./payBackLoan');
-const { servicesState } = require('./services')
-const { scheme } = require('./buddyFuctions');
+const { checkIfUserExists } = require("./buddyFuctions");
+const { RegisterUser } = require("./registeration");
+const { referalState } = require("./referrals");
+const { checkBalanceState } = require("./checkbalance");
+const { loanState } = require("./loan");
+const { welcomeState } = require("./welcome");
+const { fundWalletState } = require("./fundWallet");
+const { payBackLoanState } = require("./payBackLoan");
+const { servicesState } = require("./services");
+const { scheme } = require("./buddyFuctions");
 
 module.exports = {
-	buddymenus(menu) {
-		menu.startState({
-			run: () => {
-				// console.log(menu.args.phoneNumber, 'phoneNumber');
-				checkIfUserExists(menu.args.phoneNumber).then(
-					(val) => {
-						menu.session.set('loginStatus', '');
-						if (val.data.length < 1) {
-							menu.con('Welcome to ' + scheme + ' \n1. Register \n2. Help');
-						} else {
-							menu.con('Welcome to ' + scheme + ', \nPlease enter PIN');
-						}
-					},
-					(err) => {
-						// console.log(err);
-						menu.con('Welcome to ' + scheme + ' \n' + '\n1. Register' + '\n2. Help');
-					}
-				);
-			},
-			next: {
-				'1': 'reg',
-				'2': 'help',
-				'*[0-9]': 'welcomeState'
-			}
-		});
-		welcomeState(menu);
-		RegisterUser(menu);
-		referalState(menu);
-		checkBalanceState(menu);
-		loanState(menu);
-		fundWalletState(menu);
-		payBackLoanState(menu);
-		servicesState(menu)
-	}
+  buddymenus(menu) {
+    menu.startState({
+      run: () => {
+        // console.log(menu.args.phoneNumber, 'phoneNumber');
+        checkIfUserExists(menu.args.phoneNumber).then(
+          (val) => {
+            menu.session.set("loginStatus", "");
+            if (val.data.length < 1) {
+              menu.con("Welcome to " + scheme + " \n1. Register \n2. Help");
+            } else {
+              menu.con("Welcomes to " + scheme + ", \nPlease enter PIN");
+            }
+          },
+          (err) => {
+            // console.log(err);
+            menu.con(
+              "Welcome to " + scheme + " \n" + "\n1. Register" + "\n2. Help"
+            );
+          }
+        );
+      },
+      next: {
+        "1": "reg",
+        "2": "help",
+        "*[0-9]": "welcomeState",
+      },
+    });
+    welcomeState(menu);
+    RegisterUser(menu);
+    referalState(menu);
+    checkBalanceState(menu);
+    loanState(menu);
+    fundWalletState(menu);
+    payBackLoanState(menu);
+    servicesState(menu);
+  },
 };
